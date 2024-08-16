00dced66adafea8a4f924cacb5e4485e928b9421
Agon <agonthedev@gmail.com>
2024-08-15 21:23:40 -0700

    Read Grid Solutions

--- Assets/Code/ParxManager.cs
+++ Assets/Code/ParxManager.cs
@@ -1,13 +1,20 @@
+using System.IO;
 using UnityEngine;
 
 public class ParxManager : MonoBehaviour
 {
     public bool ClearBoard = false;
     public bool RegenerateBoard = false;
+    [Space]
+    public int solutionNo = 0;
+    public bool placeSolution = false;
+    [Space]
+    public string[] gridSolutions;
+    
     
     void Start()
     {
-        
+        LoadGridSolutions(Parx.instance.gridSize);
     }
 
     void Update()
@@ -24,5 +31,24 @@ public class ParxManager : MonoBehaviour
             RegenerateBoard = false;
         }
 
+        if ( placeSolution )
+        {
+            Parx.instance.ClearGrid();
+
+            int x = 0;
+            foreach ( char c in gridSolutions[solutionNo] )
+            {
+                if ( x >= Parx.instance.gridSize - 1 ) break;
+                Parx.instance.PlaceTree(x++, c - '0', 3);
+            }
+
+            placeSolution = false;
+        }
+    }
+
+    private void LoadGridSolutions(int gridSize)
+    {
+        string fileName = "gridSolutions_" + gridSize + ".txt";
+        gridSolutions = File.ReadAllLines(fileName);
     }
 }
